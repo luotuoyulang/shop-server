6af6c6367f250a85f3f2995102b2c8174aa9c03b
luotuoyulang <17600520726@163.com>
2020-03-20 12:50:35 +0800

    提交拦截器

--- shop-common/src/main/java/cn/luotuoyulang/common/config/InterceptorConfig.java
+++ shop-common/src/main/java/cn/luotuoyulang/common/config/InterceptorConfig.java
@@ -1,9 +1,23 @@
 package cn.luotuoyulang.common.config;
 
 import cn.luotuoyulang.common.interceptor.LoginIntercept;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description 自定义拦截器
@@ -25,4 +39,36 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 );
 
     }
+
+    //定义时间格式转换器
+    @Bean
+    public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
+
+//    @Override
+//    public void configureMessageConverters(
+//            List<HttpMessageConverter<?>> converters) {
+//        WebMvcConfigurer.super.configureMessageConverters(converters);
+//        /**
+//         * 1.需要定义一个convert转换消息的对象
+//         * 2.创建配置信息，加入配置信息：比如是否需要格式化返回的json
+//         * 3.converter中添加配置信息
+//         * 4.convert添加到converters当中
+//         */
+//        FastJsonHttpMessageConverter fastJsonHttpMessageConverter =
+//                new FastJsonHttpMessageConverter();
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(
+//                SerializerFeature.PrettyFormat
+//        );
+//        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+//        converters.add(fastJsonHttpMessageConverter);
+//    }
 }
